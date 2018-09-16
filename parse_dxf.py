8da97c54c483ef7d03197fb1d492cd6dbaa3c267
watty62 <ianwatt@gmail.com>
2018-09-16 10:17:28 +0100

    add plan

--- parse_dxf.py
+++ parse_dxf.py
@@ -4,4 +4,5 @@ dwg = ezdxf.readfile("plan of skeletons.dxf")
 modelspace = dwg.modelspace()
 images = modelspace.query("IMAGE")
 
-print images[1].get_boundary_path()
+print (images[1].get_boundary_path())
+
